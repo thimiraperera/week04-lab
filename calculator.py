8c39e0224015f67dcba7dabb3aea83251ce4e91a
Thimira Perera <thimiraperera10@gmail.com>
2026-04-26 15:13:12 +0530

    Calculator with User Inputs

--- calculator.py
+++ calculator.py
@@ -1,6 +1,21 @@
-a = 10
-b = 5
-print("Sum:", a + b)
-print("Difference:", a - b)
-print("Product:", a * b)
-print("Quotient:", a / b)
+print("Choose Operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter Operation (1/2/3/4): ")
+
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2: "))
+
+if choice == '1':
+	print(f"{num1} + {num2} = {num1 + num2}")
+elif choice == '2':
+	print(f"{num1} - {num2} = {num1 - num2}")
+elif choice == '3':
+	print(f"{num1} * {num2} = {num1 * num2}")
+elif choice == '4':
+	print(f"{num1} / {num2} = {num1 / num2}")
+else:
+	print("Invalid choice.")
